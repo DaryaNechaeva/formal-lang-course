b4d6de18f16ea77c0de5145c126a7cb759a080a6
Darya Nechaeva <necaevadara435@gmail.com>
2026-09-23 23:32:29 +0300

    feat: implement regex_to_dfa

---
+++ project/automata_utils.py
@@ -0,0 +1,6 @@
+from pyformlang.finite_automaton import DeterministicFiniteAutomaton
+from pyformlang.regular_expression import Regex
+
+
+def regex_to_dfa(regex: str) -> DeterministicFiniteAutomaton:
+    return Regex(regex).to_epsilon_nfa().to_deterministic().minimize()
